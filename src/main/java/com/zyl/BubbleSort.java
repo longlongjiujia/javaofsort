42040038154c526be9c3e37dc567da65556b8d93
zhangyanlong <zhangyanlong@jd.com>
2017-08-23 00:00:36 +0800

    1.冒泡排序

---
+++ src/main/java/com/zyl/BubbleSort.java
@@ -0,0 +1,27 @@
+package com.zyl;
+
+/**
+ *冒泡排序
+ */
+public class BubbleSort {
+
+    private static void sort(int[] tup){
+        for (int i = 0; i < tup.length - 1; i++) {
+            for (int j = 0; j < tup.length - i - 1; j++) {
+                if(tup[j] > tup[j+1]){
+                    int temp = tup[j];
+                    tup[j] = tup[j+1];
+                    tup[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] tup = {5,2,45,6,8,2,1};
+        sort(tup);
+        for (int i = 0; i < tup.length; i++) {
+            System.out.println(tup[i]);
+        }
+    }
+}
